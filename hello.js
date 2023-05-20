0d9f208523af52fec4e0309903de9084f99a68aa
sameerkewal <sameerkewal1@gmail.com>
2023-05-20 02:42:00 -0300

    yeah

--- hello.js
+++ hello.js
@@ -11,4 +11,28 @@ app.all('/test', (req, res) => {
     })
     res.redirect(`${authorizeUrl}?${queryParams}`)
 })
+
+
+app.all('/test2', async (req, res) => {
+    const code = 'P9np9kQsRs69CvWbjG29j5xtr90eUX';
+    const tokenUrl = 'https://discord.com/api/oauth2/token';
+    const tokenParams = querystring.stringify({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_TOKEN,
+        redirect_uri: 'https://testing-7c5bf.firebaseapp.com/'
+    })
+
+    const response = await fetch(tokenUrl, {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: tokenParams
+    })
+    const data = await response.json()
+    const accessToken = data.access_token;
+
+    res.send(accessToken)
+})
+
 app.listen(process.env.PORT || 3000)
