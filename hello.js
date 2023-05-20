859aaf29643610a8d7ca79fea3134c343e2f4b03
sameerkewal <106319416+sameerkewal@users.noreply.github.com>
2023-05-20 02:14:32 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log(process.env.DISCORD_CLIENT_ID)
-    res.send('Man!')
+    res.send(process.env.DISCORD_CLIENT_ID)
 })
 app.listen(process.env.PORT || 3000)
