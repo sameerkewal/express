8031fbd98c982205915d0dd8e8b118ce0913d3b3
sameerkewal <sameerkewal1@gmail.com>
2023-05-20 02:07:04 -0300

    yeah

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log(process.env.DISCORD_CLIENT_ID)
-    res.send('Man!')
+    console.log(process.env.DISCORD_CLIENT_ID);
+    res.send(process.env.DISCORD_CLIENT_ID);
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
