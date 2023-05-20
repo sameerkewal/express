d663ced741af2bab2b99ee3a553cfb7bc5c0d01d
sameerkewal <sameerkewal1@gmail.com>
2023-05-20 02:27:27 -0300

    yeah

--- hello.js
+++ hello.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const querystring = require("querystring");
 const app = express()
 app.all('/test', (req, res) => {
-    console.log('heyyyyy')
-    res.send(process.env.DISCORD_CLIENT_ID)
+    const authorizeUrl = `https://discord.com/api/oauth2/authorize`;
+    const queryParams = querystring.stringify({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        redirect_uri:'https://testing-7c5bf.firebaseapp.com/',
+        response_type: 'code',
+        scope: 'identify email'
+    })
+    res.send(`${authorizeUrl}?${queryParams}`);
+    res.redirect(`${authorizeUrl}?${queryParams}`)
 })
 app.listen(process.env.PORT || 3000)
