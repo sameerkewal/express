46c90769f0dadad7fa41ea8053f5592faa8b9dfa
sameerkewal <sameerkewal1@gmail.com>
2023-05-20 02:21:52 -0300

    yeah

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.all('/test', (req, res) => {
     console.log('heyyyyy')
     res.send(process.env.DISCORD_CLIENT_ID)
 })
